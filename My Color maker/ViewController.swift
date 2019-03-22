ce772e880c2597728129696cc81487a10d6d0d03
ManarAST <mnoooooor@gmail.com>
2019-03-22 16:53:52 +0300

    Initial Commit

---
+++ My Color maker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Color maker
+//
+//  Created by manar on 22/03/2019.
+//  Copyright © 2019 manar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
